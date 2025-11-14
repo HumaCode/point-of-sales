e3ea8d03b32caf133450e7edde019548563c3bc0
HumaCode <humaidizakaria1998@gmail.com>
2025-11-14 10:16:00 +0700

    Darkmode System

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
+import { DarkModeToggle } from "@/components/common/darkmode-toggle";
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
 
 export default function Home() {
-  return <Button>Hello</Button>;
+  return (
+    <div className="">
+      <Button className="bg-red-400 dark:bg-amber-300">Hello</Button>
+      <DarkModeToggle />
+    </div>
+  );
 }
