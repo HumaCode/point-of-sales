b51bc3285a99669e66620adb28e882719d6396e7
Humaidi Zakaria <humaidizakaria1998@gmail.com>
2025-11-18 14:59:31 +0700

    Fetch Data Tanstack-React-Query

--- src/app/(dashboard)/admin/user/page.tsx
+++ src/app/(dashboard)/admin/user/page.tsx
@@ -1,5 +1,9 @@
+import UserManagement from "./_components/user";
+
+export const metadata = {
+    title: 'WPU Cafe | User Management'
+}
+
 export default function UserManagementPage() {
-    return (
-        <div className="">User Management</div>
-    )
+    return <UserManagement/>;
 }
